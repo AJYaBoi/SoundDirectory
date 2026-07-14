3f02f38b2c2a64e172ce0339b124c89177f44620
Rat poop <105378453+AJYaBoi@users.noreply.github.com>
2026-07-14 14:02:57 -0400

    Update audio.js

--- js/audio.js
+++ js/audio.js
@@ -1,1 +1,76 @@
+/*
+==========================================================
+Mewtindew's Sound Directory
+Audio Manager
+==========================================================
+*/
 
+
+DirectoryApp.audioPlayer = null;
+
+
+
+/*
+==========================================================
+Initialize Audio System
+==========================================================
+*/
+
+function initializeAudio(){
+
+
+    document.addEventListener(
+        "play",
+        handleAudioPlay,
+        true
+    );
+
+
+}
+
+
+
+/*
+==========================================================
+Only Allow One Audio Playing
+==========================================================
+*/
+
+function handleAudioPlay(event){
+
+
+    const audio =
+        event.target;
+
+
+
+    if(
+        audio.tagName !== "AUDIO"
+    ){
+
+        return;
+
+    }
+
+
+
+    if(
+        DirectoryApp.audioPlayer &&
+
+        DirectoryApp.audioPlayer !== audio
+
+    ){
+
+        DirectoryApp.audioPlayer.pause();
+
+        DirectoryApp.audioPlayer.currentTime = 0;
+
+    }
+
+
+
+    DirectoryApp.audioPlayer =
+        audio;
+
+
+}
