3bd2c0237f2f88733ce5ace481e5fcfb8d6190eb
Rat poop <105378453+AJYaBoi@users.noreply.github.com>
2026-07-14 14:04:49 -0400

    Update router.js

--- js/router.js
+++ js/router.js
@@ -1,1 +1,168 @@
+/*
+==========================================================
+Mewtindew's Sound Directory
+URL Router
+==========================================================
+*/
 
+
+/*
+==========================================================
+Initialize Router
+==========================================================
+*/
+
+function initializeRouter(){
+
+
+    window.addEventListener(
+
+        "hashchange",
+
+        handleRouteChange
+
+    );
+
+
+}
+
+
+/*
+==========================================================
+Handle URL Changes
+==========================================================
+*/
+
+function handleRouteChange(){
+
+
+    let path =
+        getHashPath();
+
+
+
+    if(
+        DirectoryApp.folderMap.has(path)
+    ){
+
+        openFolder(
+            path,
+            false
+        );
+
+    }
+
+    else{
+
+        openFolder(
+            "",
+            false
+        );
+
+    }
+
+}
+
+
+/*
+==========================================================
+Read URL Hash
+==========================================================
+*/
+
+function getHashPath(){
+
+
+    if(
+        location.hash.length <= 1
+    ){
+
+        return "";
+
+    }
+
+
+    return decodeURIComponent(
+
+        location.hash.substring(1)
+
+    );
+
+}
+
+
+/*
+==========================================================
+Update URL
+==========================================================
+*/
+
+function updateURL(path){
+
+
+    let newHash = "";
+
+
+    if(path){
+
+        newHash =
+            "#" + encodeURIComponent(path);
+
+    }
+
+
+
+    /*
+    Avoid unnecessary history entries
+    */
+
+    if(
+        location.hash !== newHash
+    ){
+
+        history.pushState(
+            null,
+            "",
+            newHash
+        );
+
+    }
+
+}
+
+
+/*
+==========================================================
+Open Folder From URL
+==========================================================
+*/
+
+function openFromURL(){
+
+
+    const path =
+        getHashPath();
+
+
+
+    if(
+        DirectoryApp.folderMap.has(path)
+    ){
+
+        openFolder(
+            path,
+            false
+        );
+
+    }
+
+    else{
+
+        openFolder(
+            "",
+            false
+        );
+
+    }
+
+}
